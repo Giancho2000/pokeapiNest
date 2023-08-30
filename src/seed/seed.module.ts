0b1a9ce670894180f83092a6e86e6d8366b1f8e0
Giancarlo <giancarloreina125@gmail.com>
2023-08-30 17:06:36 -0500

    Seed to pokemos was added

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { PokemonModule } from 'src/pokemon/pokemon.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
+  imports:[
+    PokemonModule // Importo el modulo y tengo acceso a todos sus componentes
+  ]
 })
 export class SeedModule {}
